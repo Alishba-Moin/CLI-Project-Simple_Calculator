a067e96f4cea7ee54fd19d134785e68e25057101
Alishba-Moin <alishbamoin797@gmail.com>
2024-06-24 07:30:43 -0700

    update code

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.greenBright(`\n   <<<====================================>>>`));
+console.log(chalk.bold.redBright(`<<<=========>>>  ${chalk.redBright.bold('SIMPLE CALCULATOR')}  <<<=========>>>`));
+console.log(chalk.bold.greenBright(`   <<<====================================>>>\n`));
 const answer = await inquirer.prompt([
     {
-        message: "Enter first number",
+        message: chalk.italic.bold("Enter a first number"),
         type: "number",
         name: "FirstNumber",
     },
     {
-        message: "Enter second number",
+        message: chalk.italic.bold("Enter a second number"),
         type: "number",
         name: "SecondNumber",
     },
     {
-        message: "Select one of the operators to perform operation",
+        message: chalk.italic.bold("Select one of the operators to perform operation"),
         type: "list",
         name: "operator",
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
@@ -20,17 +24,17 @@ const answer = await inquirer.prompt([
 ]);
 // conditional statment(if-else)
 if (answer.operator === "Addition") {
-    console.log(answer.FirstNumber + answer.SecondNumber);
+    console.log(chalk.blueBright(answer.FirstNumber + answer.SecondNumber));
 }
 else if (answer.operator === "Subtraction") {
-    console.log(answer.FirstNumber - answer.SecondNumber);
+    console.log(chalk.blueBright(answer.FirstNumber - answer.SecondNumber));
 }
 else if (answer.operator === "Multiplication") {
-    console.log(answer.FirstNumber * answer.SecondNumber);
+    console.log(chalk.blueBright(answer.FirstNumber * answer.SecondNumber));
 }
 else if (answer.operator === "Division") {
-    console.log(answer.FirstNumber / answer.SecondNumber);
+    console.log(chalk.blueBright(answer.FirstNumber / answer.SecondNumber));
 }
 else {
-    console.log("Please select a valid operator");
+    console.log(chalk.red("Please select a valid operator"));
 }
